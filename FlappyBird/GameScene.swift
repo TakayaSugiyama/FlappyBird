657632069496574f191a1cda3ed3af53414e3e3e
takayasugiyama <s.takaya1027@gmail.com>
2020-09-10 06:00:20 +0900

    地面をスクロールさせる

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -14,20 +14,40 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         //背景色を設定
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        //スクロールするスプライトの親ノード
+        let scrollNode = SKNode()
+        addChild(scrollNode)
+        
         //地面の画像を読み込む
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = .nearest
         
-        //テクスチャを指定してスプライトを作成する
-        let groundSprite = SKSpriteNode(texture: groundTexture)
+        //必要な枚数を計算
+        let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
         
-        //スプライトの表示する位置を指定する
-        groundSprite.position = CGPoint(
-            x: groundTexture.size().width / 2,
-            y: groundTexture.size().height / 2
-        )
+        //スクロールするアクションを作成
+        //左方向に画像一枚スクロールさせるアクション
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+        //元の位置に戻すアクション
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
         
-        //シーンにスプライトを追加する
-        addChild(groundSprite)
+        let repeactScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        for i in 0..<needNumber {
+           //テクスチャを指定してスプライトを作成する
+           let groundSprite = SKSpriteNode(texture: groundTexture)
+           
+           //スプライトの表示する位置を指定する
+           groundSprite.position = CGPoint(
+               x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),
+               y: groundTexture.size().height / 2
+           )
+            
+           groundSprite.run(repeactScrollGround)
+           
+           //スプライトを追加する
+           scrollNode.addChild(groundSprite)
+        }
     }
 }
