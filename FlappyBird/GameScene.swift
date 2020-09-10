e60898294b51d5674bf1d54eb945ae65eef85e9f
takayasugiyama <s.takaya1027@gmail.com>
2020-09-10 21:51:02 +0900

    空をスクロールさせる

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -6,48 +6,86 @@
 //  Copyright © 2020 TakayaSugiyama. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
+    var scrollNode:SKNode!
+    
     //SKView上にシーンが表示されたときに呼ばれるメソッド
     override func didMove(to view: SKView) {
         //背景色を設定
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
         
         //スクロールするスプライトの親ノード
-        let scrollNode = SKNode()
+        scrollNode = SKNode()
         addChild(scrollNode)
         
+        setupGround()
+        setupColud()
+    }
+    
+    func setupGround() {
         //地面の画像を読み込む
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = .nearest
-        
+
         //必要な枚数を計算
         let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
-        
+
         //スクロールするアクションを作成
         //左方向に画像一枚スクロールさせるアクション
         let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
         //元の位置に戻すアクション
         let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
-        
+
         let repeactScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
-        
+
         for i in 0..<needNumber {
-           //テクスチャを指定してスプライトを作成する
-           let groundSprite = SKSpriteNode(texture: groundTexture)
+          //テクスチャを指定してスプライトを作成する
+          let sprite = SKSpriteNode(texture: groundTexture)
+          
+          //スプライトの表示する位置を指定する
+          sprite.position = CGPoint(
+              x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),
+              y: groundTexture.size().height / 2
+          )
            
-           //スプライトの表示する位置を指定する
-           groundSprite.position = CGPoint(
-               x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),
-               y: groundTexture.size().height / 2
-           )
-            
-           groundSprite.run(repeactScrollGround)
-           
-           //スプライトを追加する
-           scrollNode.addChild(groundSprite)
+          sprite.run(repeactScrollGround)
+            scrollNode.addChild(sprite)
         }
+    }
+    
+    func setupColud(){
+      //雲の画像を読み込む
+      let cloudTexture = SKTexture(imageNamed: "cloud")
+      cloudTexture.filteringMode = .nearest
+        
+      //必要な枚数を計算
+      let needCloudNumber = Int(self.frame.size.width / cloudTexture.size().width) + 2
+      
+      //スクロールするアクションを作成
+      let moveCloud = SKAction.moveBy(x: -cloudTexture.size().width, y: 0, duration: 20)
+      //元の位置に戻すアクション
+      let resetCloud = SKAction.moveBy(x: cloudTexture.size().width, y: 0, duration: 0)
+        
+      //左にスクロール->右にスクロールを無限に繰り返すアクションの作成
+      let repeatScrollCloud = SKAction.repeatForever(SKAction.sequence([moveCloud,resetCloud]))
+        
+      for i in 0..<needCloudNumber {
+        let sprite = SKSpriteNode(texture: cloudTexture)
+        sprite.zPosition = -100 //一番後ろになるようにする
+        
+        //スプライトの表示する位置を指定する
+        sprite.position = CGPoint(
+            x: cloudTexture.size().width / 2 + cloudTexture.size().width * CGFloat(i),
+            y: self.size.height - cloudTexture.size().height / 2
+        )
+        
+        //スプライトにアニメーションを設定する
+        sprite.run(repeatScrollCloud)
+        
+        //スプライトを追加する
+        scrollNode.addChild(sprite)
+      }
     }
 }
